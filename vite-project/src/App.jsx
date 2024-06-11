4c47b301d8884e3b2e35d84079a1d7fa0b5c91be
Altin <altinmusahu6@gmail.com>
2024-06-11 17:45:08 +0200

    Getting started with vite and configuration with tailwindcss

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="m-24 mt-12 flex-wrap">
+      <h1 className="m-24">Starting</h1>
+      <h1 className="m-24">Starting</h1>
+      <h1 className="m-24">Starting</h1>
+      <h1 className="m-24">Starting</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default App
